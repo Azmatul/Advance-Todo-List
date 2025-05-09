f81ae8a516ee633f1f47c32d80b5fbab6933d239
Azmatul <azmatulhaque2002@gmail.com>
2025-05-09 17:15:10 +0600

    Initial Commit

---
+++ TodoList/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db import models
+
+# Create your models here.
+class TodoList(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default= uuid.uuid4)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    is_achieve = models.BooleanField(default=False)
+
+    create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
