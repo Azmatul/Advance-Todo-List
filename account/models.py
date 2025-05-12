b3e59ba221bc86d17d62d6d42933959bf7c73931
Azmatul <azmatulhaque2002@gmail.com>
2025-05-12 15:38:35 +0600

    Adding screenshot of project

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
